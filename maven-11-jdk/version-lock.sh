1302bbb47b833f194cd2511c755958557195948d
Ronny Trommer <ronny@opennms.org>
2021-12-01 00:09:53 +0100

    Bump OpenJDK version for Maven

--- maven-11-jdk/version-lock.sh
+++ maven-11-jdk/version-lock.sh
@@ -3,6 +3,6 @@ set -u -o pipefail
 
 export VCS_SOURCE="$(git remote get-url --push origin)"
 export VCS_REVISION="$(git describe --always)"
-export BASE_IMAGE="jdk-11.0.12.b9"
+export BASE_IMAGE="quay.io/labmonkeys/openjdk:jdk-11.0.12.b25"
 export OPENJDK_11_JDK_VERSION="11.0.12+7-0ubuntu3"
 export MAVEN_VERSION="3.6.3-5"
--- maven-17-jdk/version-lock.sh
+++ maven-17-jdk/version-lock.sh
@@ -3,6 +3,6 @@ set -u -o pipefail
 
 export VCS_SOURCE="$(git remote get-url --push origin)"
 export VCS_REVISION="$(git describe --always)"
-export BASE_IMAGE="quay.io/labmonkeys/openjdk:jdk-17.b8"
+export BASE_IMAGE="quay.io/labmonkeys/openjdk:jdk-17.b23"
 export OPENJDK_17_JDK_VERSION="17+35-1"
 export MAVEN_VERSION="3.6.3-5"
