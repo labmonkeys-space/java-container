471ab76b05406ffd93e47acd49924388594ddb86
Ronny Trommer <ronny@opennms.org>
2022-01-11 13:35:43 +0100

    Update to Ubuntu:impish-20220105.b122

--- openjdk-11-jdk/version-lock.sh
+++ openjdk-11-jdk/version-lock.sh
@@ -3,5 +3,5 @@ set -u -o pipefail
 
 export VCS_SOURCE="$(git remote get-url --push origin)"
 export VCS_REVISION="$(git describe --always)"
-export BASE_IMAGE="quay.io/labmonkeys/ubuntu:impish-20211102.b72"
+export BASE_IMAGE="quay.io/labmonkeys/ubuntu:impish-20220105.b122"
 export OPENJDK_11_JDK_VERSION="11.0.13+8-0ubuntu1~21.10"
--- openjdk-17-jdk/version-lock.sh
+++ openjdk-17-jdk/version-lock.sh
@@ -3,5 +3,5 @@ set -u -o pipefail
 
 export VCS_SOURCE="$(git remote get-url --push origin)"
 export VCS_REVISION="$(git describe --always)"
-export BASE_IMAGE="quay.io/labmonkeys/ubuntu:impish-20211102.b72"
+export BASE_IMAGE="quay.io/labmonkeys/ubuntu:impish-20220105.b122"
 export OPENJDK_17_JDK_VERSION="17.0.1+12-1~21.10"
