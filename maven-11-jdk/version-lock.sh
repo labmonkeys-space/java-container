b04217172f06c769510c6d99efb52eabd378ee8e
Ronny Trommer <ronny@opennms.org>
2022-02-02 02:03:52 +0100

    Update Maven to use JDK with JAVA_HOME set

--- maven-11-jdk/version-lock.sh
+++ maven-11-jdk/version-lock.sh
@@ -3,5 +3,5 @@ set -u -o pipefail
 
 export VCS_SOURCE="$(git remote get-url --push origin)"
 export VCS_REVISION="$(git describe --always)"
-export BASE_IMAGE="quay.io/labmonkeys/openjdk:jdk-11.0.14.b88"
+export BASE_IMAGE="quay.io/labmonkeys/openjdk:jdk-11.0.14.b106"
 export MAVEN_VERSION="3.6.3-5"
