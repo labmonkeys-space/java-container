a68aa03309a6a4a1a00572235b3fbf45b295e2bd
Ronny Trommer <ronny@opennms.org>
2022-02-16 13:37:41 +0100

    Update Ubuntu base image dependencies

--- openjdk-11-jdk/version-lock.sh
+++ openjdk-11-jdk/version-lock.sh
@@ -3,5 +3,5 @@ set -u -o pipefail
 
 export VCS_SOURCE="$(git remote get-url --push origin)"
 export VCS_REVISION="$(git describe --always)"
-export BASE_IMAGE="quay.io/labmonkeys/ubuntu:jammy-20220101.b123"
+export BASE_IMAGE="quay.io/labmonkeys/ubuntu:jammy-20220130.b140"
 export OPENJDK_11_JDK_VERSION="11.0.14+9-0ubuntu2"
--- openjdk-17-jre/version-lock.sh
+++ openjdk-17-jre/version-lock.sh
@@ -3,5 +3,5 @@ set -u -o pipefail
 
 export VCS_SOURCE="$(git remote get-url --push origin)"
 export VCS_REVISION="$(git describe --always)"
-export BASE_IMAGE="quay.io/labmonkeys/ubuntu:jammy-20220101.b123"
+export BASE_IMAGE="quay.io/labmonkeys/ubuntu:jammy-20220130.b140"
 export OPENJDK_17_JRE_VERSION="17.0.2+8-1"
