9887eb57055fa3876c0924e3e07dc80889326aa6
Ronny Trommer <ronny@opennms.org>
2023-01-31 18:57:03 +0100

    Update OS container

--- openjdk-17-jdk/version-lock.sh
+++ openjdk-17-jdk/version-lock.sh
@@ -7,5 +7,5 @@ DATE="$(date -u +"%Y-%m-%dT%H:%M:%SZ")"
 export VCS_SOURCE
 export VCS_REVISION
 export DATE
-export BASE_IMAGE="quay.io/labmonkeys/ubuntu:jammy-20221130.b251"
+export BASE_IMAGE="quay.io/labmonkeys/ubuntu:jammy-20230126.b263"
 export OPENJDK_17_JDK_VERSION="17.0.5+8-2ubuntu1~22.04"
